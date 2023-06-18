1acba0ad167e5bed7258ed7f1fee71cf1bb1c6e0
Clementine <clemwanjiru@gmail.com>
2023-06-18 14:43:13 +0300

    addition javascript form

---
+++ form.js
@@ -0,0 +1,6 @@
+function addnumbers() {
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    var sum =num1+num2;
+    document.getElementById("sum").value = sum;
+}
